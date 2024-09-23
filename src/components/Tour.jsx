4411842dd1637c138731232c800f038e4b28c318
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-09-23 22:53:09 +0530

    finally

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -13,10 +13,10 @@ const Tour = ({tour,RemoveTour}) => {
       <h2>{name}</h2>
       <img src={image} alt={name} />
       <h2>${price}</h2>
-      
+
        <p  id={`tour-item-para-${id}`}>
         {isExpand ? info : `${info.substring(0, 200)}...`}
-      <button onClick={toggleButton}>
+      <button  id={`see-more-${id}`} onClick={toggleButton}>
         {isExpand? 'see Less': 'show more'}
       </button>
       </p>
